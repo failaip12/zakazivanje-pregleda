45da8668dd5dc08a85ea2910a4c69bd298b750c9
failaip12 <86018517+failaip12@users.noreply.github.com>
2025-06-25 16:57:20 +0200

    init

---
+++ src/main/java/com/ambulanta/zakazivanje_pregleda/ZakazivanjePregledaApplication.java
@@ -0,0 +1,13 @@
+package com.ambulanta.zakazivanje_pregleda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakazivanjePregledaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZakazivanjePregledaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambulanta/zakazivanje_pregleda/ZakazivanjePregledaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambulanta.zakazivanje_pregleda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZakazivanjePregledaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
